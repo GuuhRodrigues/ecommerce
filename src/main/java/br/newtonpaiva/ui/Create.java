357ac834f8f95829321074f225b5293563406f75
12116534 <guhrodriguesads@gmail.com>
2023-06-02 20:49:20 -0300

    feat: cria conexao do ecommerce com o bando de dados

--- src/main/java/br/newtonpaiva/ui/Create.java
+++ src/main/java/br/newtonpaiva/ui/Create.java
@@ -1,9 +1,11 @@
 package br.newtonpaiva.ui;
 
 import br.newtonpaiva.dominio.Categoria;
-import jakarta.persistence.EntityManager;
-import jakarta.persistence.EntityManagerFactory;
-import jakarta.persistence.Persistence;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
 
 public class Create {
     public static void main(String[] args) {
